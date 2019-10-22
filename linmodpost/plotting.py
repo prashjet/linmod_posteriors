b5e72bbc5dd4ff864f794b840b420387b9857ec9
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2019-10-22 22:04:39 +0200

    define MidpointNormalize class

--- linmodpost/plotting.py
+++ linmodpost/plotting.py
@@ -1,6 +1,30 @@
 import numpy as np
+
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 from matplotlib.collections import LineCollection
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from . samplers import Samples
+
+# set the colormap and centre the colorbar
+class MidpointNormalize(colors.Normalize):
+	"""
+	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+
+    taken from e.g. https://github.com/matplotlib/matplotlib/issues/14892
+	"""
+	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+		self.midpoint = midpoint
+		colors.Normalize.__init__(self, vmin, vmax, clip)
+
+	def __call__(self, value, clip=None):
+		# I'm ignoring masked values and all kinds of edge cases to make a
+		# simple example...
+		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
 class Plotter:
 
