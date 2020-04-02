6536d2995b22e493bcf43c813f976512b5be724b
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2020-04-02 12:13:13 +0200

    expand init

--- linmodpost/__init__.py
+++ linmodpost/__init__.py
@@ -1,1 +1,1 @@
-from linmodpost import beta_priors, data, stat_models, distribution_functions, dimensionality_reduction, model_grids, samplers, samples, plotting
+from linmodpost import beta_priors, data, stat_models, distribution_functions, dimensionality_reduction, model_grids, samplers, samples, plotting, hierarchical_samplers
